65d9a60d043a3cf12829c40fd2a8ed151bdc3351
DwQ7 <Qindw1004@@163.com>
2022-03-26 01:19:40 +0800

    修改转换页面脚本

--- public/javascripts/transition.js
+++ public/javascripts/transition.js
@@ -1,16 +1,37 @@
+import session from "./session.js";
+import history from "./pojo/history.js";
+import date from "./pojo/date.js"
+// let obj = new  history.History(1,2,3);
+// history.histories.push(obj);
+// session.setSession("history",history.histories);
+// console.log(history.histories);
+// console.log(sessionStorage);
+// console.log(session.getSession("history"));
 $(function () {
+    let fileNames = [];
     console.log(sessionStorage.username);
-
     if(sessionStorage.username){
         $('#user').attr('href','/user').text(sessionStorage.username);;
     }
     $( "#trans_effect" ).hide();
-    $('#upload').on('click',function () {
-        Fade();
-        setTimeout(function() {
-           Out();
-        }, 500 );
+    $('#pdfFile').change(function () {
+        fileNames = $('#pdfFile').prop('files');
+        for(let i = 0 ; i < fileNames.length ; i ++){
+            let filename  = fileNames[i].name;
+            console.log(filename);
+            $('.left_main').append(filename + '\n');
+        }
     })
+    function getFileName(routerName){
+        let index = routerName.lastIndexOf('\\')
+        let filename = routerName.substr(index + 1)
+        return filename;
+    }
+    function fileJudge(filename){
+        let index=filename.lastIndexOf(".");
+        let lastname = filename.substring(index,filename.length)
+        return lastname;
+    }
     function Fade() {
         let selectedEffect = "blind";
         let options = {};
@@ -37,13 +58,43 @@ $(function () {
         }
         // 运行特效
         $( "#trans_effect" ).show( selectedEffect, options, 1000);
-    };
-
+    }
     //回调函数
     function callback_out() {
         setTimeout(function() {
             $( "#trans_effect" ).removeAttr( "style" ).fadeOut();
         }, 1000 );
     };
-
+    $("#upload").on('click',function () {
+        console.log(sessionStorage.getItem('username'));
+        let pdfFile = $('#pdfFile')[0].files[0];
+        if(pdfFile){
+            Fade();
+            setTimeout(function() {
+                Out();
+            }, 500 );
+            const formData =  new FormData($('#uploadForm')[0]);
+            formData.append("username",sessionStorage.username);
+            $.ajax({
+                type: 'post',
+                url: '/apis/first/PdfToEpub/upload/one',
+                data: formData,
+                processData: false, //不处理数据
+                contentType: false,
+                success: function (data) {
+                    console.log(data);
+                    let obj = new history.History(
+                        fileNames[0].name,
+                        data,
+                        date.getFormatDate()
+                    )
+                    history.histories.push(obj);
+                    session.setSession("history",history.histories);
+                    window.alert(data);
+                }
+            });
+        }else{
+            window.alert("请选择文件");
+        }
+    });
 })
