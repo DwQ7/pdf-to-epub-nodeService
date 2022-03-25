8df428f0095f5d48bc18d5b85ff9e5d39a189aa6
DwQ7 <Qindw1004@@163.com>
2022-03-26 01:16:53 +0800

    修改主页面脚本

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -5,31 +5,7 @@ $(function () {
         $('#user').attr('href','/user');
     }
 
-    $("#upload").on('click',function () {
-        window.location.href = '/transition'
-        console.log(sessionStorage.getItem('username'));
-        let pdfFile = $('#pdfFile')[0].files[0];
-        if(pdfFile){
-            const formData =  new FormData($('#uploadForm')[0]);
-            formData.append("username","admin");
-            $.ajax({
-                type: 'post',
-                url: '/apis/first/PdfToEpub/upload/one',
-                data: formData,
-                processData: false, //不处理数据
-                contentType: false,
-                dataType:'json',
-                success: function (res) {
-                    console.log(res);
-                },
-                error: function (){
-                    console.log('err');
-                }
-            });
-        }else{
-            window.alert("请选择文件");
-        }
-    });
+
     $('.choose_file').on('click',function () {
         window.location.href = "/transition"
     })
