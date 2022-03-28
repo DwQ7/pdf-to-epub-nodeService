999b3132d8676c8d4a3ef91a46a344381e161dfc
DwQ7 <Qindw1004@@163.com>
2022-03-28 16:52:40 +0800

    登录注册脚本修改

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,11 +1,11 @@
 $(function () {
-    console.log(sessionStorage.username);
+    //更新页面信息
+    // console.log(sessionStorage.username);
     $('#user').text(sessionStorage.username);
     if(sessionStorage.username){
         $('#user').attr('href','/user');
     }
-
-
+    //事件监听
     $('.choose_file').on('click',function () {
         window.location.href = "/transition"
     })
--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,11 +1,5 @@
 $(function () {
-    // let str='';  //定义一个空变量用来存储密码
-    // $('#password').keyup(function(){
-    //     let value=$(this).val();	//输入的时候获取输入框的值
-    //     str+=value.substr(value.length-1,1); //获取最后一个字符加到到str,因为除了最后一个字符，其他的已经为*
-    //     $(this).val(value.replace(/./g,'*')) //输入框内容全部变为*
-    //     console.log(str); //str即为输入框的内容
-    // })
+    //事件监听
     $("#btnLogin").on('click',function () {
         console.log(1);
         const username = $("#email").val();
@@ -23,6 +17,7 @@ $(function () {
                 if(data !== 'err'){
                     console.log(data);
                     sessionStorage.setItem('username',data);
+                    sessionStorage.setItem('fileNum','1');
                     window.location.href ='/user';
                 }else{
                     window.alert('error');
