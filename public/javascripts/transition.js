596b5eb2803e121c03721f8b527d1f07b670bf34
DwQ7 <Qindw1004@@163.com>
2022-03-28 16:55:14 +0800

    转换界面脚本

--- public/javascripts/transition.js
+++ public/javascripts/transition.js
@@ -1,6 +1,8 @@
-import session from "./session.js";
+import session from "./utils/session.js";
 import history from "./pojo/history.js";
 import date from "./pojo/date.js"
+import animation from "./utils/animation.js";
+import fileUtil from "./utils/fileUtil.js";
 // let obj = new  history.History(1,2,3);
 // history.histories.push(obj);
 // session.setSession("history",history.histories);
@@ -8,70 +10,33 @@ import date from "./pojo/date.js"
 // console.log(sessionStorage);
 // console.log(session.getSession("history"));
 $(function () {
+    //选择的文件
     let fileNames = [];
-    console.log(sessionStorage.username);
-    if(sessionStorage.username){
-        $('#user').attr('href','/user').text(sessionStorage.username);;
-    }
+    //隐藏动画
     $( "#trans_effect" ).hide();
-    $('#pdfFile').change(function () {
+    //更新页面信息
+    if(sessionStorage.username){
+        $('#user').attr('href','/user').text(sessionStorage.username);
+    }
+    //修改并渲染文件信息
+    $('#pdfFile').on('change',function () {
         fileNames = $('#pdfFile').prop('files');
         for(let i = 0 ; i < fileNames.length ; i ++){
             let filename  = fileNames[i].name;
             console.log(filename);
             $('.left_main').append(filename + '\n');
         }
+
     })
-    function getFileName(routerName){
-        let index = routerName.lastIndexOf('\\')
-        let filename = routerName.substr(index + 1)
-        return filename;
-    }
-    function fileJudge(filename){
-        let index=filename.lastIndexOf(".");
-        let lastname = filename.substring(index,filename.length)
-        return lastname;
-    }
-    function Fade() {
-        let selectedEffect = "blind";
-        let options = {};
-        if ( selectedEffect === "scale" ) {
-            options = { percent: 0 };
-        } else if ( selectedEffect === "size" ) {
-            options = { to: { width: 200, height: 60 } };
-        }
-        $( "#up_effect" ).hide( selectedEffect, options, 1000, callback_fade );
-    };
-    function callback_fade() {
-        setTimeout(function() {
-            $( "#trans_effect" ).hide();
-            $( "#up_effect" ).removeAttr( "style" ).hide().fadeIn();
-        }, 2000 );
-    };
-    function Out() {
-        let selectedEffect = "blind"
-        let options = {};
-        if ( selectedEffect === "scale" ) {
-            options = { percent: 100 };
-        } else if ( selectedEffect === "size" ) {
-            options = { to: { width: 280, height: 185 } };
-        }
-        // 运行特效
-        $( "#trans_effect" ).show( selectedEffect, options, 1000);
-    }
-    //回调函数
-    function callback_out() {
-        setTimeout(function() {
-            $( "#trans_effect" ).removeAttr( "style" ).fadeOut();
-        }, 1000 );
-    };
+    //事件监听
     $("#upload").on('click',function () {
         console.log(sessionStorage.getItem('username'));
         let pdfFile = $('#pdfFile')[0].files[0];
+
         if(pdfFile){
-            Fade();
+            animation.Fade();
             setTimeout(function() {
-                Out();
+                animation.Out();
             }, 500 );
             const formData =  new FormData($('#uploadForm')[0]);
             formData.append("username",sessionStorage.username);
@@ -82,15 +47,33 @@ $(function () {
                 processData: false, //不处理数据
                 contentType: false,
                 success: function (data) {
-                    console.log(data);
+                    // console.log(data);
                     let obj = new history.History(
                         fileNames[0].name,
                         data,
                         date.getFormatDate()
                     )
-                    history.histories.push(obj);
-                    session.setSession("history",history.histories);
-                    window.alert(data);
+                    let num = parseInt(sessionStorage.getItem('fileNum'));
+                    session.setSession(num,obj);
+                    let newFileNum = (++num).toString();
+                    console.log(newFileNum);
+                    sessionStorage.setItem('fileNum',newFileNum);
+                    window.alert('转换成功，点击下载');
+                    $('#pdfFile').val(''); //让input标签的值刷新，防止无法上传之前上传过的文件
+                    window.location.href = '/download';
+                },
+                xhr: function() {
+                    let xhr = new XMLHttpRequest();
+                    //使用XMLHttpRequest.upload监听上传过程，注册progress事件，打印回调函数中的event事件
+                    xhr.upload.addEventListener('progress', function (e) {
+                        // console.log(e);
+                        //loaded代表上传了多少
+                        //total代表总数为多少
+                        let progressRate = (e.loaded / e.total) * 100 + '%';
+                        //通过设置进度条的宽度达到效果
+                        $('.progress > div').css('width', progressRate);
+                    })
+                    return xhr;
                 }
             });
         }else{
